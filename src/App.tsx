6ce45e31dabc0d372b526c8898eac8a1cea9eb3f
kartheek542 <kartheekdivvela@gmail.com>
2025-02-23 16:49:23 +0530

    Calling backend API to test connectivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,33 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import apiClient from "./API/apiClient";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [backendMessage, setBackendMessage] = useState("");
+    const [clicked, setClicked] = useState(false);
+
+    const handleButtonClick = async () => {
+        if (clicked) {
+            setClicked(false);
+            setBackendMessage("");
+        } else {
+            const response = await apiClient.get("/api/hello");
+            if (response.status === 200) {
+                setBackendMessage(response.data.message);
+            } else {
+                setBackendMessage("Error occured");
+            }
+            setClicked(true);
+        }
+    };
 
     return (
         <>
+            <h1>Welcome to Hostcode</h1>
             <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
+                <button onClick={handleButtonClick}>Hit Backend</button>
+                {clicked && <p>Message from Backend is {backendMessage}</p>}
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
